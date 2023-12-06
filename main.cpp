57a3ea5a3c96e0e2ca63a84905e1b5a803daba1e
nsaririan <80706549+nsaririan@users.noreply.github.com>
2023-12-06 12:07:58 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -3,135 +3,157 @@
 #include <vector>
 #include <string>
 #include <sstream>
-#include <list>
+#include <algorithm>
+#include <stdexcept>
 
 using namespace std;
 
-class HashTable {
-private:
-   
-    int size;
-    int num = 0;
-    vector<list<pair<string, int>>> table;
-    float maxLoadFac = 1.5;
-
-    int hashFunction(const string key) const {
-        int num = 0;
-        for (unsigned int i = 0; i < key.length(); i++) {
-            num += static_cast<int>(key.at(i)) * 7 ^ (i);
-        }
-        int index = num % size;
-        return index;
-    }
-
+class TreeNode {
 public:
-    // Constructor
-    HashTable(int initialSize = 10) : size(initialSize), table(initialSize) {}
+    string state;
+    int sightingsCount;
+    vector<TreeNode*> children;
 
-    // Insert a key-value pair into the hash table
-    void insert(const string& key) {
-        int index = hashFunction(key);
-        for (auto& entry : table[index]) {
-            if (entry.first == key) {
-                // Update value if key already exists
-                entry.second++;
-                return;
-            }
+    TreeNode(const string& state) : state(state), sightingsCount(0) {}
+
+    // Insert UFO sighting into the tree
+    void insertSighting(const string& state) {
+        auto it = find_if(children.begin(), children.end(),
+                          [&state](const TreeNode* node) {
+                              return node->state == state;
+                          });
+
+        if (it != children.end()) {
+            // State exists, increment count
+            (*it)->sightingsCount++;
         }
-        // If key doesn't exist, add a new key-value pair
-        table[index].push_back({key, 1});
-        num++;
-
-        if ((float)(num / size) == maxLoadFac) {
-            rehash();
+        else {
+            // State doesn't exist, create a new node
+            TreeNode* newNode = new TreeNode(state);
+            newNode->sightingsCount++;
+            children.push_back(newNode);
         }
     }
 
-    // Get the value associated with a key
-    int get(const string& key) const {
-        int index = hashFunction(key);
-        for (const auto& entry : table[index]) {
-            if (entry.first == key) {
-                return entry.second;
-            }
-        }
-        throw out_of_range("Key not found");
-    }
+    // Remove child node using state abbreviation
+    void removeChild(const string& stateAbbreviation) {
+        auto it = remove_if(children.begin(), children.end(),
+                            [&stateAbbreviation](TreeNode* node) {
+                                return node->state == stateAbbreviation;
+                            });
 
-    void rehash() {
-        int newSize = size * 2;  // Double the size
-        vector<list<pair<string, int>>> newTable(newSize);
-
-        // Insert existing values into the new table
-        for (const auto& entries : table) {
-            for (const auto& entry : entries) {
-                int newIndex = hashFunction(entry.first);
-                newTable[newIndex].push_back(entry);
-            }
+        // Delete removed nodes
+        for (auto iter = it; iter != children.end(); ++iter) {
+            delete *iter;
         }
 
-        // Update the hash table with the new size and content
-        size = newSize;
-        table = move(newTable);
-        newTable.clear();
+        // Erase removed nodes from vector
+        children.erase(it, children.end());
     }
 
 
-    // Remove a key-value pair from the hash table
-    void remove(const string& key) {
-        int index = hashFunction(key);
-        auto& entries = table[index];
-        for (auto it = entries.begin(); it != entries.end(); ++it) {
-            if (it->first == key) {
-                entries.erase(it);
-                return;
-            }
+    // Perform DFS traversal of tree and print top states
+    void printTopStates(int n) const {
+        // Sort children using sightings count
+        vector<TreeNode*> sortedChildren = children;
+        sort(sortedChildren.begin(), sortedChildren.end(),
+             [](const TreeNode* a, const TreeNode* b) {
+                 return a->sightingsCount > b->sightingsCount;
+             });
+
+        for (int i = 0; i < min(n, static_cast<int>(sortedChildren.size())); ++i) {
+            cout << sortedChildren[i]->state << ": " << sortedChildren[i]->sightingsCount << " sightings\n";
         }
-        throw out_of_range("Key not found");
+    }
+
+    // Destructor: free memory taken up by tree nodes
+    ~TreeNode() {
+        for (TreeNode* child : children) {
+            delete child;
+        }
     }
 };
 
-
-
 int main() {
-	// File pointer 
-	fstream fin;
+    fstream fin;
 
-	// Open file 
-	fin.open("ufo_sightings.csv", ios::in);
-	
-	if (!fin.is_open()) {
-		cout << "File could not be read";
-	}
+    fin.open("/Users/niloufarsaririan/CLionProjects/ufo/ufo_sightings.csv", ios::in);
 
+    if (!fin.is_open()) {
+        cout << "File could not be read";
+        return 1;
+    }
 
-	vector<string> row;
-	vector<vector<string>> col;
-	string line, temp;
-	while (fin) {
-		row.clear();
+    vector<string> row;
+    vector<vector<string>> col;
+    string line, temp;
+    while (fin) {
+        row.clear();
 
-		getline(fin, line);
+        getline(fin, line);
 
-		stringstream lineStream(line);
-		string cell;
+        stringstream lineStream(line);
+        string cell;
 
-		while (getline(lineStream, cell, ','))
-		{
-			row.push_back(cell);
-		}
-		col.push_back(row);
-	}
+        while (getline(lineStream, cell, ','))
+        {
+            row.push_back(cell);
+        }
+        col.push_back(row);
+    }
 
-    HashTable states(10);
+    // Create n-ary tree
+    TreeNode* root = new TreeNode("Root");
 
+    // Load UFO sightings data into n-ary tree
+    for (unsigned int i = 0; i < col.size(); i++) {
+        if (col.at(i).size() > 1) {
+            root->insertSighting(col.at(i).at(1));
+        }
+    }
 
-	for (unsigned int i = 0; i < col.size(); i++) {
-		for (unsigned int j = 0; j < col.at(i).size(); j++) {
-			states.insert(col.at(i).at(1));
-		}
-	}
-	
+    // Menu
+    int choice;
 
-	return 0;
+    do {
+        cout << "Menu:\n";
+        cout << "1. Print top N states\n";
+        cout << "2. Remove state\n";
+        cout << "3. Quit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        // Clear input buffer, had to look this up: cite
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        switch (choice) {
+            case 1: {
+                int n;
+                cout << "Enter the number of top states to print: ";
+                cin >> n;
+                root->printTopStates(n);
+                break;
+            }
+            case 2: {
+                string stateToRemove;
+                cout << "Enter the state to remove: ";
+                cin >> stateToRemove;
+                root->removeChild(stateToRemove);
+                cout << "State removed.\n";
+                break;
+            }
+            case 3:
+                cout << "Goodbye!\n";
+                break;
+            default:
+                cout << "Invalid choice. Please try again.\n";
+        }
+    } while (choice != 3);
+
+    // Cleanup: Release memory taken up by tree nodes
+    delete root;
+
+    return 0;
 }
+
